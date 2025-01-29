61df2ee29bbe497a6577fbaec0416add740db50e
Srijan Upadhyay <srjnupadhyay@gmail.com>
2025-01-29 18:42:01 +0530

    Bried made

--- Payment's Pipeline/Payment's Pipeline/frontend/components/Notification.js
+++ Payment's Pipeline/Payment's Pipeline/frontend/components/Notification.js
@@ -3,26 +3,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Snackbar, Alert } from '@mui/material';
 
+const NOTIFICATION_TYPES = {
+    SUCCESS: 'success',
+    ERROR: 'error',
+    WARNING: 'warning',
+    INFO: 'info'
+};
+
 const Notification = ({ open, message, severity, onClose }) => {
-  return (
-    <Snackbar
-      open={open}
-      autoHideDuration={6000}
-      onClose={onClose}
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-    >
-      <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
-        {message}
-      </Alert>
-    </Snackbar>
-  );
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={6000} // Seconds Initiatewd
+            onClose={onClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+            <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+                {message}
+            </Alert>
+        </Snackbar>
+    );
 };
 
 Notification.propTypes = {
-  open: PropTypes.bool.isRequired,
-  message: PropTypes.string.isRequired,
-  severity: PropTypes.oneOf(Object.values(NOTIFICATION_TYPES)),
-  onClose: PropTypes.func.isRequired
+    open: PropTypes.bool.isRequired,
+    message: PropTypes.string.isRequired,
+    severity: PropTypes.oneOf(Object.values(NOTIFICATION_TYPES)),
+    onClose: PropTypes.func.isRequired
 };
 
 export default Notification;
